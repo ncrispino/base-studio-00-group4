f6395c857b0f0800593a1f9a1b80e8b5160d4e27
jackhansell <jackhansell@172.16.182.104>
2022-01-20 13:54:41 -0600

    Computer Science come help

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
  
 	public static void main(String args[ ])  {
-		System.out.println("computer science");
+		System.out.println("computer science is fun");
 	}
 }
